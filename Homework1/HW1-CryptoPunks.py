810d05c2ed1474a0bbd8f55d60dd678fe807faf4
gav-conn <gavinconnolly1@gmail.com>
2023-02-21 11:03:53 +0000

    Fixed merge conflicts

--- Homework1/HW1-CryptoPunks.py
+++ Homework1/HW1-CryptoPunks.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import requests
-from dateutil.parser import parse
 from bs4 import BeautifulSoup
 
-class_list = pd.read_csv("ClassList1.csv", sep=",",)
-class_number = class_list.loc[class_list["Student ID"] == 18308483, "Class Number"].item()
-
+my_student_number = 18308483
 BaseStr = "https://www.larvalabs.com/cryptopunks/details/"
+
+class_list = pd.read_csv("ClassList1.csv", sep=",")
+class_number = class_list.loc[class_list["Student ID"] == my_student_number, "Class Number"].item()
+
 page = requests.get(BaseStr + str(class_number))
 soup = BeautifulSoup(page.content, 'html.parser')
 
-test = soup.find(class_="col-md-10 col-md-offset-1")
-result = test.find_all('a')
-print(result)
-# for row in rows:
-#     cols = row.find_all('td')
-#     if cols:
-#         cols = [ele.text.strip() for ele in cols]
-#         print(cols[4] + ' : ' + cols[3])
+result_set = soup.find_all("div", class_="col-md-4")
+attributes = []
+
+for link in result_set:
+    attributes.extend(link.find('a'))
+
+attributes = attributes[1:]
+print(attributes)
--- Homework1/HW1-YahooFinance.py
+++ Homework1/HW1-YahooFinance.py
@@ -2,12 +2,14 @@ import pandas as pd
 import yfinance as yf
 import mplfinance as mpf
 
+my_student_number = 18308483
+
 class_list = pd.read_csv("ClassList1.csv", sep=",")
+ticker_name = class_list.loc[class_list["Student ID"] 
+                             == my_student_number, "Assigned Ticker"].item()
 
-ticker_name = class_list.loc[class_list["Student ID"] == 18308483, "Assigned Ticker"].item()
 yticker = yf.Ticker(ticker_name)
-
-BA = yticker.history(period="1y") # max, 1y, 3mo
+BA = yticker.history(period="1y")
 
 mpf.plot(BA, type='candle', style = 'charles', 
     title='Boeing - Price (Past Year)', xlabel = 'Date', ylabel='Price ($)')
